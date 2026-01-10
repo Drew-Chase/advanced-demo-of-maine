b5584f4bd3897471822309c45340fe311343ca5c
Shroototem <xxxioxx69@gmail.com>
2026-01-09 22:44:23 -0500

    Made the animated link modular for better useability.

--- src/components/AnimatedLink.tsx
+++ src/components/AnimatedLink.tsx
@@ -1,17 +1,37 @@
-import { Link } from "@heroui/react";
+import { cn, Link } from "@heroui/react";
 import { motion } from "framer-motion";
 import { Icon } from "@iconify-icon/react";
 import React from "react";
 
-export default function AnimatedLink() {
+type AnimatedLinkProps = {
+    text: string;
+    href: string;
+    color?: "primary" | "secondary";
+    classNames?: { link?: string, motion?: string };
+    fontSize?: number;
+}
+
+export default function AnimatedLink(props: AnimatedLinkProps) {
     const [hover, setHover] = React.useState(false);
     return (
-        <div className="relative">
-            <Link href="/building-with-purpose" className="text-white text-xs w-fit relative z-10 hover:text-black transition-colors duration-300" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-                <p className="z-10 pl-2">Learn more about us</p>
+        <div className="relative w-fit">
+
+            <Link href={props.href}
+                style={{ fontSize: props.fontSize || 12 }}
+                className={cn(props.classNames?.link, "text-white text-xs w-fit relative z-10 hover:text-black transition-colors duration-300")}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+            >
+                <p className="z-10 pl-2">{props.text}</p>
                 <Icon icon="solar:arrow-right-broken" className="ml-4 text-2xl" />
             </Link>
-            <motion.div className="pointer-events-none absolute bottom-0 w-[160px] bg-secondary" initial={{ height: "2px" }} animate={{ height: hover ? "24px" : "2px", transition: hover ? { type: "spring", bounce: 0.6 } : { type: "tween" } }} />
+
+            <motion.div
+                className={cn(props.classNames?.motion, "w-full pointer-events-none absolute bottom-0 data-[color=primary]:bg-primary data-[color=secondary]:bg-secondary")}
+                initial={{ height: "2px" }}
+                animate={{ height: hover ? "24px" : "2px", transition: hover ? { type: "spring", bounce: 0.6 } : { type: "tween" } }}
+                data-color={props.color}
+            />
         </div>
     );
 }
--- src/components/BuildingWithPurpose.tsx
+++ src/components/BuildingWithPurpose.tsx
@@ -6,7 +6,7 @@ export default function BuildingWithPurpose() {
             <div className="ml-10 md:ml-40 flex flex-col gap-4">
                 <h1 className="text-4xl">Building with Purpose</h1>
                 <p className="text-wrap w-[50%] min-w-[300px] max-w-[600px]">Building With Purpose is our shared commitment to deliver exceptional  results on every project, making a lasting, positive impact on our  clients, communities, people, business partners and our industry through the things we build and do.</p>
-                <AnimatedLink />
+                <AnimatedLink text="Learn more about us" href="/building-with-purpose" color="secondary" fontSize={12} />
             </div>
             <div className="w-full h-full absolute inset-0 -z-10">
                 <div className="absolute inset-0 w-full h-full bg-black/30 z-10" />
